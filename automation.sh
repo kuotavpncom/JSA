6608d28ba5b9be80187c5b53ec2b2b9ae5a2dcd9
kuotavpncom <kuotavpn@gmail.com>
2025-01-24 15:26:36 +0700

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -26,7 +26,7 @@ printf $stdin | xargs -I{} echo "{}/*&filter=mimetype:text/javascript&somevar=" 
 ## only wayback as of now
 
 printf "Fetching URLs for 404 js files from wayback..\n"
-cat tmp/gau${random_str}.txt | cut -d '?' -f1 | cut -d '#' -f1 | sort -u | xargs -I{} sh -c automation/./404_js_wayback.sh {} | tee -a tmp/creds_search${random_str}.txt >/dev/null
+cat tmp/gau${random_str}.txt | cut -d '?' -f1 | cut -d '#' -f1 | sort -u | xargs -I{} sh -c automation/404_js_wayback.sh {} | tee -a tmp/creds_search${random_str}.txt >/dev/null
 
 
 ## Classic crawling. It could give different results than subjs tool
@@ -37,8 +37,8 @@ printf $stdin | hakrawler -u -subs -insecure -d 2 | grep '\.js' | tee tmp/spider
 ## Searching for URLs in github, - that could give some unique results, too
 ## python one-liner - for clear domain matching
 
-printf 'Searching for URLs in GH..\n'
-printf ${stdin} | python3 -c "import re,sys; str0=str(sys.stdin.readlines()); str1=re.search('(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', str0);  print(str1.group(0)) if str1 is not None else exit()" | xargs -I{} python3 github-endpoints.py -d {} | grep '\.js' | tee tmp/gh${random_str}.txt >/dev/null
+# printf 'Searching for URLs in GH..\n'
+# printf ${stdin} | python3 -c "import re,sys; str0=str(sys.stdin.readlines()); str1=re.search('(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]', str0);  print(str1.group(0)) if str1 is not None else exit()" | xargs -I{} python3 github-endpoints.py -d {} | grep '\.js' | tee tmp/gh${random_str}.txt >/dev/null
     
     
 ## sorting out all the results
@@ -51,10 +51,10 @@ cat tmp/subjs${random_str}.txt tmp/gau${random_str}.txt tmp/gh${random_str}.txt 
 
 ## extracting js files from js files
 printf "Printing deep-level js files..\n"
-cat tmp/all_js_files${random_str}.txt | parallel --gnu --pipe -j 15 "python3 automation/js_files_extraction.py | tee -a tmp/all_js_files${random_str}.txt"
+cat tmp/all_js_files${random_str}.txt | parallel --gnu --pipe -j 15 "python3 automation/js_files_extraction.py | httpx -sc -silent | tee -a tmp/all_js_files${random_str}.txt"
 
 printf "Searching for endpoints..\n"
-cat tmp/all_js_files${random_str}.txt | parallel --gnu --pipe -j 15 "python3 automation/endpoints_extraction.py | tee -a tmp/all_endpoints${random_str}.txt"
+cat tmp/all_js_files${random_str}.txt | parallel --gnu --pipe -j 15 "python3 automation/endpoints_extraction.py -sc -silent | tee -a tmp/all_endpoints${random_str}.txt"
 cat tmp/all_endpoints${random_str}.txt | sort -u  | tee tmp/all_endpoints_unique${random_str}.txt >/dev/null
 
 ## credentials checking
@@ -62,11 +62,14 @@ cat tmp/all_endpoints${random_str}.txt | sort -u  | tee tmp/all_endpoints_unique
 printf "Checking our js files for sweet credentials.."
 cat tmp/all_js_files${random_str}.txt tmp/creds_search${random_str}.txt | parallel --gnu -j 15 "nuclei -t templates/credentials-disclosure-all.yaml -no-color -silent -target {}"
 
+printf "Checking our js files for sweet credentials filess.."
+cat tmp/all_js_files${random_str}.txt tmp/creds_search${random_str}.txt | parallel --gnu -j 15 "nuclei -t templates/credentials-disclosure-all.yaml -no-color -silent -target {}"
+
 
 ## parameters bruteforcing with modified Arjun
 
 printf "Arjun parameters discovery.."
-cat tmp/all_endpoints_unique${random_str}.txt | parallel -j 15 "python3 Arjun/arjun.py -f Arjun/db/params.txt -t 12 --get -u {}"
+cat tmp/all_endpoints_unique${random_str}.txt | parallel -j 15 "arjun -t 12 --get -u {}"
 
 
 rm tmp/subjs${random_str}.txt tmp/gau${random_str}.txt tmp/spider${random_str}.txt tmp/gh${random_str}.txt tmp/all_js_files${random_str}.txt tmp/all_endpoints${random_str}.txt tmp/all_endpoints_unique${random_str}.txt
